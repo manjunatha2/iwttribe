a5fc74478e6e3666fa0efae633e631a9a0652fc3
manjunatha2 <manjunath.tantry@investwithtribe.com>
2022-11-12 22:49:18 +0530

    fixes for logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,24 +1,24 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Link } from 'components/link';
-import LogoSvg from 'components/icons/logo';
+import { jsx } from "theme-ui";
+import { Link } from "components/link";
+import LogoPng from "components/icons/tribe.png";
 
 export default function Logo({ isSticky, footer, ...props }) {
   return (
     <Link path="/" sx={styles.logo} {...props}>
-      <LogoSvg />
+      <img src={LogoPng} height={60} width={60} />
     </Link>
   );
 }
 const styles = {
   logo: {
-    alignItems: 'center',
-    cursor: 'pointer',
-    display: 'inline-flex',
-    svg: {
-      height: 'auto',
-      width: [128, null, '100%'],
-    },
+    alignItems: "center",
+    cursor: "pointer",
+    display: "inline-flex",
+    // svg: {
+    //   height: "auto",
+    //   width: [128, null, "100%"],
+    // },
   },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO
-          title="Startup hosting provider landing"
-          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
+          title="Investwithtribe"
+          description="Invest Better Invest Together"
         />
         <Banner />
         <Services />
