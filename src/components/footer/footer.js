2fc2d8b259c9b0076526bf53badf09deff222c1f
manjunatha2 <manjunath.tantry@investwithtribe.com>
2022-11-12 22:49:23 +0530

    Fixes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,11 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, Box, Text, Container } from 'theme-ui';
-import Logo from 'components/logo';
-import { Link } from 'components/link';
-import FooterWidget from 'components/footer-widget';
-import { menuItems, footerNav } from './footer.data';
-import { rgba } from 'polished';
+import { jsx, Box, Text, Container } from "theme-ui";
+import Logo from "components/logo";
+import { Link } from "components/link";
+import FooterWidget from "components/footer-widget";
+import { menuItems, footerNav } from "./footer.data";
+import { rgba } from "polished";
 
 export default function Footer() {
   return (
@@ -22,7 +22,7 @@ export default function Footer() {
               <Link path="#!">Privacy</Link>
             </Box>
             <Text as="p" sx={styles.copyright}>
-              Copyright by {new Date().getFullYear()} RedQ, Inc
+              Copyright by {new Date().getFullYear()} Better Wealth Techologies
             </Text>
           </Box>
           {menuItems.map(({ id, title, items }) => (
@@ -36,69 +36,69 @@ export default function Footer() {
 
 const styles = {
   footerTopInner: {
-    gap: [30, 30, 50, '20px 50px', 17, 50],
-    display: ['grid'],
+    gap: [30, 30, 50, "20px 50px", 17, 50],
+    display: ["grid"],
     gridTemplateColumns: [
-      'repeat(2, 1fr)',
-      'repeat(2, 1fr)',
-      'repeat(2, 1fr)',
-      'repeat(4, 1fr)',
-      'repeat(5, 1fr)',
+      "repeat(2, 1fr)",
+      "repeat(2, 1fr)",
+      "repeat(2, 1fr)",
+      "repeat(4, 1fr)",
+      "repeat(5, 1fr)",
     ],
   },
   footerInner: {
     borderTop: `1px solid #D9E0E7`,
-    display: ['block', 'block', 'flex'],
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '35px 0 40px',
+    display: ["block", "block", "flex"],
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "35px 0 40px",
   },
   about: {
-    gridRow: ['3/4', '3/4', '1/1', '3/4', 'unset'],
-    gridColumn: ['1/3', '1/3', '1/2', '1/5', 'unset'],
+    gridRow: ["3/4", "3/4", "1/1", "3/4", "unset"],
+    gridColumn: ["1/3", "1/3", "1/2", "1/5", "unset"],
   },
   logo: {
-    textAlign: ['center', 'center', 'left', 'center'],
+    textAlign: ["center", "center", "left", "center"],
   },
   terms: {
-    display: ['flex'],
+    display: ["flex"],
     alignItems: [
-      'center',
-      'center',
-      'center',
-      'center',
-      'flex-start',
-      'center',
+      "center",
+      "center",
+      "center",
+      "center",
+      "flex-start",
+      "center",
     ],
-    flexDirection: ['row', 'row', 'row', 'row', 'column', 'row'],
+    flexDirection: ["row", "row", "row", "row", "column", "row"],
     justifyContent: [
-      'center',
-      'center',
-      'flex-start',
-      'center',
-      'center',
-      'flex-start',
+      "center",
+      "center",
+      "flex-start",
+      "center",
+      "center",
+      "flex-start",
     ],
     mt: [4],
     a: {
-      color: 'heading',
+      color: "heading",
     },
     span: {
       display: [
-        'inline-flex',
-        'inline-flex',
-        'inline-flex',
-        'inline-flex',
-        'none',
-        'inline-flex',
+        "inline-flex",
+        "inline-flex",
+        "inline-flex",
+        "inline-flex",
+        "none",
+        "inline-flex",
       ],
-      m: ['0 10px'],
+      m: ["0 10px"],
     },
   },
   copyright: {
-    color: rgba('#0F2137', 0.6),
-    fontSize: ['14px'],
+    color: rgba("#0F2137", 0.6),
+    fontSize: ["14px"],
     mt: [3],
-    textAlign: ['center', 'center', 'left', 'center', 'left', 'left'],
+    textAlign: ["center", "center", "left", "center", "left", "left"],
   },
 };
